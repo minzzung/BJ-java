cb4819318029282067d5707af2cfade66271c2dc
minzzung <dullini0205@gmail.com>
2022-03-07 23:52:24 +0900

    upload CH3test

--- CH4/test03_1110/Main.java
+++ CH4/test03_1110/Main.java
@@ -2,7 +2,22 @@ package CH4.test03_1110;
 
 import java.util.Scanner;
 public class Main {
-    public static void main(String arg[]){
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		in.close();
         
-    }
+		int count = 0;
+		int copy = n;
+        
+		while (true) {
+			n = ((n % 10) * 10) + (((n / 10) + (n % 10)) % 10);
+			count++;
+ 
+			if (copy == n) {
+				break;
+			}
+		}
+		System.out.println(count);
+	}
 }
