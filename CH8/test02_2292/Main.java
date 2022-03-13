4f0d7c3ee0e4e09fc934ae2b8f584b76e81a43af
minzzung <dullini0205@gmail.com>
2022-03-13 23:40:20 +0900

    javatest upload 22.03.13

--- CH8/test02_2292/Main.java
+++ CH8/test02_2292/Main.java
@@ -1,10 +1,23 @@
 package CH8.test02_2292;
 
+//쉽지만 방법을 못찾은 문제
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        System.out.println((a/6)+1);
+        int count=1;
+        int sum = 2;
+        if (a ==1){
+            System.out.println(1);
+        }
+        else{
+            while(sum<=a){
+                sum = sum +(6*count);
+                count++;
+            }
+            System.out.println(count);
+        }
+        
     }
 }
